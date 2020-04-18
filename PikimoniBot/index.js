f0b543ba770aed934e7dd7b6eac7e2c19844243c
a34alefeliza <a.feliziani@crif.com>
2020-04-18 10:25:46 +0200

    export function

--- PikimoniBot/index.js
+++ PikimoniBot/index.js
@@ -10,78 +10,82 @@ const Topic = require('./model/topic');
 const Question = require('./model/question');
 const qaService = require('./service/qa');
 
-const bot = new Telegraf('1081398486:AAFs2L1OOtRTi321vuNwUrgn7ddMlNoWD4g', { webhookReply: true })
-bot.telegram.setWebhook('https://pikimoni-bot.azurewebsites.net/api/PikimoniBot');
+module.exports = async function (context) {
+    context.log('JavaScript trigger function processed a request.');
 
-// Log to console middleware
-bot.use((ctx, next) => {
-	console.log('Message from: ' + ctx.from.username)
-	return next()
-})
+    const bot = new Telegraf('1081398486:AAFs2L1OOtRTi321vuNwUrgn7ddMlNoWD4g', { webhookReply: true })
+    bot.telegram.setWebhook('https://pikimoni-bot.azurewebsites.net/api/PikimoniBot');
 
-bot.catch((err) => {
-	console.log('Unexpected error: ', err);
-})
+    // Log to console middleware
+    bot.use((ctx, next) => {
+        console.log('Message from: ' + ctx.from.username)
+        return next()
+    })
 
-const connectionString = 'mongodb://pkm-mongo:DLwjqWBdEJzsv64WkfpxHAIKS92rgKHX853pLkt0bNL75uYe5pV9oTxz1LdrlDp1eCmOveyhEKWTMaPhIPLEoA==@pkm-mongo.mongo.cosmos.azure.com:10255/?ssl=true&appName=@pkm-mongo@';
-//const connectionString = 'mongodb://localhost:27018/bot';
-var mongoOpts = { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-};
-mongoose.connect(connectionString, mongoOpts).then(function (dbInstance) {
+    bot.catch((err) => {
+        console.log('Unexpected error: ', err);
+    })
 
-    // Handler factories
-    const { enter, leave } = Stage
+    const connectionString = 'mongodb://pkm-mongo:DLwjqWBdEJzsv64WkfpxHAIKS92rgKHX853pLkt0bNL75uYe5pV9oTxz1LdrlDp1eCmOveyhEKWTMaPhIPLEoA==@pkm-mongo.mongo.cosmos.azure.com:10255/?ssl=true&appName=@pkm-mongo@';
+    //const connectionString = 'mongodb://localhost:27018/bot';
+    var mongoOpts = { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    };
+    mongoose.connect(connectionString, mongoOpts).then(function (dbInstance) {
 
-    const newTopicScene = new WizardScene(
-        'new-topic',
-        ctx => {
-            ctx.reply('Ok, you want to create a new topic. Enter the name:')
-            return ctx.wizard.next();
-        },
-        ctx => {
-            var topic = new Topic({name:ctx.message.text});
-            topic.save().then(function(topic){
-                ctx.reply(topic.name + ' created successfully.');
-                return ctx.scene.leave();
-            })
-        }
-    );
-    const browseTopicsScene = new WizardScene(
-        'new-question',
-        ctx => {
-            qaService.showTopics(ctx);
-            return ctx.wizard.next();
-        },
-        ctx => {
-            ctx.wizard.state.topic = ctx.callbackQuery.data.split('/')[1];
-            ctx.reply('Write the question that you would like being answerd:')
-            return ctx.wizard.next();
-        },
-        ctx => {
-            ctx.wizard.state.question = ctx.message.text;
-            var question = new Question(ctx.wizard.state);
-            question.save().then(function(question){
-                console.log(question.toJSON());
-                ctx.reply('question submitted successfully. It will be answered as soon as possible.');
-                return ctx.scene.leave();
-            })
-        }
-    );
+        // Handler factories
+        const { enter, leave } = Stage
 
-    const stage = new Stage([newTopicScene, browseTopicsScene], {});
+        const newTopicScene = new WizardScene(
+            'new-topic',
+            ctx => {
+                ctx.reply('Ok, you want to create a new topic. Enter the name:')
+                return ctx.wizard.next();
+            },
+            ctx => {
+                var topic = new Topic({name:ctx.message.text});
+                topic.save().then(function(topic){
+                    ctx.reply(topic.name + ' created successfully.');
+                    return ctx.scene.leave();
+                })
+            }
+        );
+        const browseTopicsScene = new WizardScene(
+            'new-question',
+            ctx => {
+                qaService.showTopics(ctx);
+                return ctx.wizard.next();
+            },
+            ctx => {
+                ctx.wizard.state.topic = ctx.callbackQuery.data.split('/')[1];
+                ctx.reply('Write the question that you would like being answerd:')
+                return ctx.wizard.next();
+            },
+            ctx => {
+                ctx.wizard.state.question = ctx.message.text;
+                var question = new Question(ctx.wizard.state);
+                question.save().then(function(question){
+                    console.log(question.toJSON());
+                    ctx.reply('question submitted successfully. It will be answered as soon as possible.');
+                    return ctx.scene.leave();
+                })
+            }
+        );
+
+        const stage = new Stage([newTopicScene, browseTopicsScene], {});
 
 
-    bot.use(session())
-    bot.use(stage.middleware());
-    bot.start((ctx) => qaService.showHelp(ctx))
-    bot.hears('help', (ctx) => qaService.showHelp(ctx))
-    bot.action('topics', (ctx) => qaService.showTopics(ctx))
-    bot.action('newQuestion', (ctx) => ctx.scene.enter('new-question'))
-    bot.action(qaService.topicFn, (ctx) => qaService.showQuestions(ctx))
-    bot.action(qaService.questionFn, (ctx) => qaService.showAnswer(ctx))
-    bot.command('newtopic', (ctx) => ctx.scene.enter('new-topic'))
-    bot.launch()
+        bot.use(session())
+        bot.use(stage.middleware());
+        bot.start((ctx) => qaService.showHelp(ctx))
+        bot.hears('help', (ctx) => qaService.showHelp(ctx))
+        bot.action('topics', (ctx) => qaService.showTopics(ctx))
+        bot.action('newQuestion', (ctx) => ctx.scene.enter('new-question'))
+        bot.action(qaService.topicFn, (ctx) => qaService.showQuestions(ctx))
+        bot.action(qaService.questionFn, (ctx) => qaService.showAnswer(ctx))
+        bot.command('newtopic', (ctx) => ctx.scene.enter('new-topic'))
+        bot.launch()
 
-})
+    })
+}
